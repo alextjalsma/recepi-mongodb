b1294123ffd0bcdced1776549b5d49365c5f1034
atjalsma@home.nl <atjalsma@home.nl>
2019-12-22 20:17:33 +0100

    Initial commit

---
+++ src/main/java/alex/tjalsma/recepi/RecepiApplication.java
@@ -0,0 +1,13 @@
+package alex.tjalsma.recepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecepiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alex/tjalsma/recepi/RecepiApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.tjalsma.recepi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

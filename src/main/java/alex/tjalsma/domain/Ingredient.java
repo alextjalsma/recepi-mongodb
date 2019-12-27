01d3a7c48bb2268a895827cfd9f1c4cd7a5529e6
atjalsma@home.nl <atjalsma@home.nl>
2019-12-27 07:40:18 +0100

    added Ingredient

---
+++ src/main/java/alex/tjalsma/domain/Ingredient.java
@@ -0,0 +1,51 @@
+package alex.tjalsma.domain;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descreption;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescreption() {
+        return descreption;
+    }
+
+    public void setDescreption(String descreption) {
+        this.descreption = descreption;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
--- src/main/java/alex/tjalsma/domain/Recipe.java
+++ src/main/java/alex/tjalsma/domain/Recipe.java
@@ -1,6 +1,7 @@
 package alex.tjalsma.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private byte[] image;

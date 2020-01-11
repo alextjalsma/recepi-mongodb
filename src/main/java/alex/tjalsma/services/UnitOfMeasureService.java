4ad6b06fb8811c6e2152bbde519d2df71ec7018f
atjalsma@home.nl <atjalsma@home.nl>
2020-01-11 14:35:24 +0100

    added new and delete functionality for Ingredient

---
+++ src/main/java/alex/tjalsma/services/UnitOfMeasureService.java
@@ -0,0 +1,10 @@
+package alex.tjalsma.services;
+
+import alex.tjalsma.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
